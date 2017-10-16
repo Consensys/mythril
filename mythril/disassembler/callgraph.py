223ad38338160912ce1fb3c202d9b85001671280
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-10-16 16:32:57 +0700

    Change command line arguments

--- mythril/disassembler/callgraph.py
+++ mythril/disassembler/callgraph.py
@@ -1,64 +1,9 @@
-import graphviz as gv
-
-
-styles = {
-    'graph': {
-        'overlap': 'false',
-        'fontsize': '16',
-        'fontcolor': 'white',
-        'bgcolor': '#333333',
-        'concentrate':'true',
-    },
-    'nodes': {
-        'fontname': 'Helvetica',
-        'shape': 'box',
-        'fontcolor': 'white',
-        'color': 'white',
-        'style': 'filled',
-        'fillcolor': '#006699',
-    },
-    'edges': {
-        'style': 'dashed',
-        'dir': 'forward',
-        'color': 'white',
-        'arrowhead': 'normal',
-        'fontname': 'Courier',
-        'fontsize': '12',
-        'fontcolor': 'white',
-    }
-}
-
-def apply_styles(graph, styles):
-    graph.graph_attr.update(
-        ('graph' in styles and styles['graph']) or {}
-    )
-    graph.node_attr.update(
-        ('nodes' in styles and styles['nodes']) or {}
-    )
-    graph.edge_attr.update(
-        ('edges' in styles and styles['edges']) or {}
-    )
-    return graph
-
+from laser.ethereum import svm, cfg
 
 def generate_callgraph(disassembly, file):
 
-	graph = gv.Graph(format='svg')
+    _svm = svm.SVM(disassembly)
 
-	index = 0
+    _svm.sym_exec()
 
-	for block in disassembly.blocks:
-		easm = block.get_easm().replace("\n", "\l")
-
-		graph.node(str(index), easm)
-		index += 1
-
-	for xref in disassembly.xrefs:
-
-		graph.edge(str(xref[0]), str(xref[1]))
-
-
-	graph = apply_styles(graph, styles)
-
-	graph.render(file)
-
+    cfg.generate_callgraph(_svm, file)
