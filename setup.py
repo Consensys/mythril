ef47f42968518387c2e12474801f1f6a0f5b4bda
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-10-21 09:38:03 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ security community.
 setup(
     name='mythril',
 
-    version='0.5.8',
+    version='0.5.9',
 
     description='A reversing and bug hunting framework for the Ethereum blockchain',
     long_description=long_description,
