35ce848e2db7ff828cac16c10b96980eb70051fa
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-10-28 08:55:32 +0700

    Fix graph generator bug

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ security community.
 setup(
     name='mythril',
 
-    version='0.6.0',
+    version='0.6.1',
 
     description='A reversing and bug hunting framework for the Ethereum blockchain',
     long_description=long_description,
