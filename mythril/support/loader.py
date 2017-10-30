e7ed3c2b7dcdf0c31f76ad43a76962b3744f95eb
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-10-30 16:34:22 +0700

    Add dynamic loader (experimental)

--- mythril/support/loader.py
+++ mythril/support/loader.py
@@ -1,11 +1,43 @@
 from mythril.ether.ethcontract import ETHContract
+import logging
+import re
 
 class DynLoader:
 
-	def __init__(self, eth):
-		self.eth = eth
+    def __init__(self, eth):
+        self.eth = eth
 
-	def dynld(self, address):
+    def dynld(self, contract_address, dependency_address):
 
-		contract = ETHContract(self.eth.eth_getCode(address), name=address, address=address)
-		return contract.as_dict()
+        logging.info("Dynld at contract " + contract_address + ": "  + dependency_address)
+
+        # Hack-ish
+
+        m = re.match(r'(0x[0-9a-fA-F]{40})', dependency_address)
+
+        if (m):
+            dependency_address = m.group(1)
+
+        else:
+            m = re.search(r'storage_(\d+)', dependency_address)
+
+            if (m):
+                idx = int(m.group(1))
+                logging.info("Dynamic contract address at storage index " + str(idx))
+
+                dependency_address = "0x" + self.eth.eth_getStorageAt(contract_address, position=idx, block='latest')[26:]
+
+            else:
+                logging.info("Unable to resolve address.")
+                return None
+
+
+        logging.info("Dependency address: " +  dependency_address)
+
+        code = self.eth.eth_getCode(dependency_address)
+
+        if (code == "0x"):
+            return None
+        else:
+            contract = ETHContract(self.eth.eth_getCode(dependency_address), name=dependency_address, address=dependency_address)
+            return contract.as_dict()
