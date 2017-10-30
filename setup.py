1108511890816ab0379066545be7f86e7f0880a1
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-10-30 16:35:59 +0700

    Update setup.py and requirements

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ security community.
 setup(
     name='mythril',
 
-    version='0.6.3',
+    version='0.7.0',
 
     description='A reversing and bug hunting framework for the Ethereum blockchain',
     long_description=long_description,
@@ -256,7 +256,7 @@ setup(
         'web3',
         'ZODB>=5.3.0',
         'z3-solver>=4.5',
-        'laser-ethereum==0.1.13',
+        'laser-ethereum==0.2.0',
         'requests',
         'BTrees'
     ],
