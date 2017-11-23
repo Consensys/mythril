60cd332739a92522d9677e30b08faedab4570002
Bernhard Mueller <bernhard.mueller11@gmail.com>
2017-11-23 13:18:44 +0700

    Update setup.py and requirements

--- setup.py
+++ setup.py
@@ -256,7 +256,7 @@ setup(
         'web3',
         'ZODB>=5.3.0',
         'z3-solver>=4.5',
-        'laser-ethereum==0.2.16',
+        'laser-ethereum==0.2.17',
         'requests',
         'BTrees'
     ],
