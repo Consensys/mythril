5fb0a2d5d31fe5345f01c5552226fb9d682dec1b
Bernhard Mueller <bernhardmueller@Cerebral-Cortex.local>
2018-01-09 13:33:15 +0700

    Add informational messages

--- mythril/analysis/security.py
+++ mythril/analysis/security.py
@@ -1,6 +1,7 @@
 from mythril.analysis.report import Report
 from mythril.analysis import modules
 import pkgutil
+import logging
 
 
 def fire_lasers(statespace):
@@ -11,7 +12,10 @@ def fire_lasers(statespace):
     for loader, name, is_pkg in pkgutil.walk_packages(modules.__path__):
         _modules.append(loader.find_module(name).load_module(name))
 
+    logging.info("Starting analysis")
+
     for module in _modules:
+        logging.info("Executing " + str(module))
         issues += module.execute(statespace)
 
     report = Report()
