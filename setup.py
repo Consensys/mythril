fead5342589e2d4b9d37817fbcbb6331027ca631
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-03-07 18:44:14 +0700

    LASER rewrite

--- setup.py
+++ setup.py
@@ -254,7 +254,7 @@ Credit
 setup(
     name='mythril',
 
-    version='0.12.6',
+    version='0.13.0',
 
     description='Security analysis tool for Ethereum smart contracts',
     long_description=long_description,
@@ -291,7 +291,7 @@ setup(
         'web3',
         'ZODB>=5.3.0',
         'z3-solver>=4.5',
-        'laser-ethereum==0.4.4',
+        'laser-ethereum==0.5.0',
         'requests',
         'BTrees',
         'py-solc'
