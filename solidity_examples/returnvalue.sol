62eb0b96774193ca6f7344ace31ee8d7544a03fc
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-03-12 17:24:21 +0700

    Finalize return value example

--- solidity_examples/returnvalue.sol
+++ solidity_examples/returnvalue.sol
@@ -2,18 +2,12 @@ contract ReturnValue {
 
   address callee = 0xE0F7e56e62b4267062172495D7506087205A4229;
 
-  function call1() returns (uint256) {
+  function callnotchecked()  {
     callee.call();
   }
 
-  function call2() returns (uint256) {
-
-    
-  }
-
-  function call3() returns (uint256) {
-
-    
+  function callchecked()  {
+  	require(callee.call());
   }
 
 }
