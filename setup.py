bf419c89b581e12596ed22b2db8da34fb257ba54
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-03-19 12:37:02 +0700

    Bump version number

--- setup.py
+++ setup.py
@@ -254,7 +254,7 @@ Credit
 setup(
     name='mythril',
 
-    version='0.14.0',
+    version='0.14.1',
 
     description='Security analysis tool for Ethereum smart contracts',
     long_description=long_description,
