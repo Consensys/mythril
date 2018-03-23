765b450fa422deea554c6070e5ad104ac1f8d8f6
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-03-23 12:34:22 +0700

    Add -m/--modules command line argument

--- mythril/analysis/security.py
+++ mythril/analysis/security.py
@@ -4,18 +4,19 @@ import pkgutil
 import logging
 
 
-def fire_lasers(statespace):
+def fire_lasers(statespace, module_names=None):
 
     issues = []
     _modules = []
-    
+
     for loader, name, is_pkg in pkgutil.walk_packages(modules.__path__):
         _modules.append(loader.find_module(name).load_module(name))
 
     logging.info("Starting analysis")
 
     for module in _modules:
-        logging.info("Executing " + str(module))
-        issues += module.execute(statespace)
+        if not module_names or module.__name__ in module_names:
+            logging.info("Executing " + str(module))
+            issues += module.execute(statespace)
 
     return issues
