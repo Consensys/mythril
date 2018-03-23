412b36790139b95c91c388a14611a93013d5a288
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-03-23 19:36:33 +0700

    Update solver.py

--- mythril/analysis/solver.py
+++ mythril/analysis/solver.py
@@ -1,6 +1,5 @@
 from z3 import Solver, simplify, sat
 from mythril.exceptions import UnsatError
-import re
 
 
 def get_model(constraints):
@@ -25,10 +24,7 @@ def pretty_print_model(model):
     for d in model.decls():
 
         try:
-            if (re.match(r'^calldata_.*_(\d)+', d.name())):
-                condition = "%064x" % model[d].as_long()
-            else:
-                condition = str(model[d].as_long())
+            condition = "0x%x" % model[d].as_long()
         except:
             condition = str(simplify(model[d]))
 
