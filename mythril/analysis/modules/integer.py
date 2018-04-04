85a941a2553d688d75aeb0afb63bbe2dbfe73c08
Joran Honig <joran.honig@gmail.com>
2018-04-05 00:21:55 +0200

    Reformat code to separate integer under and overflow modules

--- mythril/analysis/modules/integer.py
+++ mythril/analysis/modules/integer.py
@@ -7,7 +7,6 @@ import re
 import copy
 import logging
 
-
 '''
 MODULE DESCRIPTION:
 
@@ -17,7 +16,11 @@ For every SUB instruction, check if there's a possible state where op1 > op0.
 
 
 def execute(statespace):
-
+    """
+    Executes analysis module for integer underflow
+    :param statespace: Statespace to analyse
+    :return: Found issues
+    """
     logging.debug("Executing module: INTEGER")
 
     issues = []
@@ -26,52 +29,64 @@ def execute(statespace):
         node = statespace.nodes[k]
 
         for state in node.states:
-
-            instruction = state.get_current_instruction()
-
-            if(instruction['opcode'] == "SUB"):
-
-                stack = state.mstate.stack
-
-                op0 = stack[-1]
-                op1 = stack[-2]
-
-                constraints = copy.deepcopy(node.constraints)
-
-                if type(op0) == int and type(op1) == int:
-                    continue
-
-                if (re.search(r'calldatasize_', str(op0))) \
-                    or (re.search(r'256\*.*If\(1', str(op0), re.DOTALL) or re.search(r'256\*.*If\(1', str(op1), re.DOTALL)) \
-                    or (re.search(r'32 \+.*calldata', str(op0), re.DOTALL) or re.search(r'32 \+.*calldata', str(op1), re.DOTALL)):
-
-                    # Filter for patterns that contain bening nteger underflows.
-
-                    # Pattern 1: (96 + calldatasize_MAIN) - (96), where (96 + calldatasize_MAIN) would underflow if calldatasize is very large.
-                    # Pattern 2: (256*If(1 & storage_0 == 0, 1, 0)) - 1, this would underlow if storage_0 = 0
-
-                    continue
-
-                logging.debug("[INTEGER_UNDERFLOW] Checking SUB " + str(op0) + ", " + str(op1) + " at address " + str(instruction['address']))
-
-                allowed_types = [int, BitVecRef, BitVecNumRef]
-
-                if type(op0) in allowed_types and type(op1) in allowed_types:
-                    constraints.append(UGT(op1,op0))
-
-                    try:
-
-                        model = solver.get_model(constraints)
-
-                        issue = Issue(node.contract_name, node.function_name, instruction['address'], "Integer Underflow", "Warning")
-
-                        issue.description = "A possible integer underflow exists in the function " + node.function_name + ".\n" \
-                            "The subtraction may result in a value < 0."
-
-                        issue.debug = solver.pretty_print_model(model)
-                        issues.append(issue)
-
-                    except UnsatError:
-                        logging.debug("[INTEGER_UNDERFLOW] no model found") 
+            logging.debug("Checking for integer underflow")
+            issues += _check_integer_underflow(state, node)
+            logging.debug("Checking for integer overflow")
+            issues += _check_integer_overflow(state, node)
 
     return issues
+
+
+def _check_integer_overflow(state, node):
+    return []
+
+
+def _check_integer_underflow(state, node):
+    issues = []
+    instruction = state.get_current_instruction()
+
+    if instruction['opcode'] == "SUB":
+
+        stack = state.mstate.stack
+
+        op0 = stack[-1]
+        op1 = stack[-2]
+
+        constraints = copy.deepcopy(node.constraints)
+
+        # Filter for patterns that contain bening nteger underflows.
+
+        # Pattern 1: (96 + calldatasize_MAIN) - (96), where (96 + calldatasize_MAIN) would underflow if calldatasize is very large.
+        # Pattern 2: (256*If(1 & storage_0 == 0, 1, 0)) - 1, this would underlow if storage_0 = 0
+        if type(op0) == int and type(op1) == int:
+            return
+        if re.search(r'calldatasize_', str(op0)):
+            return
+        if re.search(r'256\*.*If\(1', str(op0), re.DOTALL) or re.search(r'256\*.*If\(1', str(op1), re.DOTALL):
+            return
+        if re.search(r'32 \+.*calldata', str(op0), re.DOTALL) or re.search(r'32 \+.*calldata', str(op1), re.DOTALL):
+            return
+
+        logging.debug("[INTEGER_UNDERFLOW] Checking SUB {0}, {1} at address {2}".format(str(op0), str(op1),
+                                                                                        str(instruction['address'])))
+
+        allowed_types = [int, BitVecRef, BitVecNumRef]
+
+        if type(op0) in allowed_types and type(op1) in allowed_types:
+            constraints.append(UGT(op1, op0))
+
+            try:
+
+                model = solver.get_model(constraints)
+
+                issue = Issue(node.contract_name, node.function_name, instruction['address'], "Integer Underflow",
+                              "Warning")
+
+                issue.description = "A possible integer underflow exists in the function " + node.function_name + ".\n" \
+                                                                                                                  "The subtraction may result in a value < 0."
+
+                issue.debug = solver.pretty_print_model(model)
+                issues.append(issue)
+
+            except UnsatError:
+                logging.debug("[INTEGER_UNDERFLOW] no model found")
