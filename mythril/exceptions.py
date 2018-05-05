446a7e6346d220fbe837b9deb172939f6b3063c7
tintinweb <tintinweb@oststrom.com>
2018-05-06 00:33:14 +0200

    refactored __main__ creating a base Mythril interface class  the idea is that in the future this will be the main entry point to  script mythril (see mythril/mythril.py - main interface) created user facing interface subpackage and moved console logic to cli.py changed myth to import from interfaces/cli (clean interface) changed __main__ to import from interfaces/cli (clean interface)

--- mythril/exceptions.py
+++ mythril/exceptions.py
@@ -1,9 +1,19 @@
-class MythrilBaseException(Exception): pass
+class MythrilBaseException(Exception): 
+    pass
 
-class CompilerError(MythrilBaseException): pass
 
-class UnsatError(MythrilBaseException): pass
+class CompilerError(MythrilBaseException): 
+    pass
 
-class NoContractFoundError(MythrilBaseException): pass
 
-class CriticalError(MythrilBaseException): pass
+class UnsatError(MythrilBaseException): 
+    pass
+
+
+class NoContractFoundError(MythrilBaseException): 
+    pass
+
+
+class CriticalError(MythrilBaseException):
+    pass
+
