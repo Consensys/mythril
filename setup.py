363d16c9e3db11ca3e14b6352aa218eba407fc02
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-05-14 10:33:24 +0700

    Bump laser version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 # Package version (vX.Y.Z). It must match git tag being used for CircleCI
 # deployment; otherwise the build will failed.
 
-VERSION = "v0.17.7"
+VERSION = "v0.17.8"
 
 class VerifyVersionCommand(install):
   """Custom command to verify that the git tag matches our version"""
@@ -307,7 +307,7 @@ setup(
         'ethereum>=2.3.0',
         'ZODB>=5.3.0',
         'z3-solver>=4.5',
-        'laser-ethereum>=0.17.7',
+        'laser-ethereum>=0.17.8',
         'requests',
         'BTrees',
         'py-solc',
