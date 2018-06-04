f0d4f81b7aad4bff6012a1a722763d4e5cac6db2
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-06-04 16:14:13 +0700

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 # Package version (vX.Y.Z). It must match git tag being used for CircleCI
 # deployment; otherwise the build will failed.
 
-VERSION = "v0.17.12"
+VERSION = "v0.17.13"
 
 class VerifyVersionCommand(install):
   """Custom command to verify that the git tag matches our version"""
