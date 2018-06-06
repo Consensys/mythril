756bd1ebe72b5aee5e9efeb30f8cd6469995c2d6
Josh Asplund <joshasplund@gmail.com>
2018-06-06 13:07:26 -0500

    Simplifies Circle config and coverage

--- coverage_report.sh
+++ coverage_report.sh
@@ -7,6 +7,7 @@ rm -rf ./tests/testdata/outputs_current/
 mkdir -p ./tests/testdata/outputs_current/
 rm -rf coverage_html_report
 
-coverage run -m unittest discover -p "*_test.py"
-coverage html
-open coverage_html_report/index.html
+py.test \
+    --cov=mythril \
+    --cov-config=tox.ini \
+    --cov-report=html:coverage_html_report \
