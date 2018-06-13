c3131a579dd152b702485146eafd4ecdf1c35a5a
Konrad Weiss <konrad.weiss13@gmail.com>
2018-06-13 11:30:39 +0200

    Add annotation files such as the wrapper in front of the analysis and the annotation class structures

---
+++ mythril/analysis/modules/dummy.py
@@ -0,0 +1,33 @@
+from mythril.analysis.report import Issue
+import logging
+
+
+'''
+    To print content of the statespace after it was build.
+'''
+
+def print_obj(obj):
+    print()
+    print(obj)
+    # print(dir(obj))
+    print()
+
+
+def execute(statespace):
+
+    logging.debug("Executing module: Transaction End")
+
+    issues = []
+
+    for k in statespace.nodes:
+        node = statespace.nodes[k]
+
+        for state in node.states:
+
+            instruction = state.get_current_instruction()
+
+            if(instruction['opcode'] == "STOP"):
+                print_obj(state.environment.active_account.storage)
+                # print("opc: {}, add: {} {}".format(instruction['opcode'], instruction['address'], instruction['argument'] if 'argument' in instruction else ""))
+
+    return issues
