7aabbd2472ff326ffb8244acb34098958e851ee1
Joran Honig <joran.honig@gmail.com>
2018-08-14 11:28:44 +0200

    Change log message

--- mythril/laser/ethereum/svm.py
+++ mythril/laser/ethereum/svm.py
@@ -85,7 +85,7 @@ class LaserEVM:
         logging.info("%d nodes, %d edges, %d total states", len(self.nodes), len(self.edges), self.total_states)
         for code, coverage in self.coverage.items():
             cov = reduce(lambda sum_, val: sum_ + 1 if val else sum_, coverage[1]) / float(coverage[0]) * 100
-            logging.info("Achieved {} coverage for contract".format(cov))
+            logging.info("Achieved {} coverage for code: {}".format(cov, code))
 
     def exec(self, create=False):
         for global_state in self.strategy:
