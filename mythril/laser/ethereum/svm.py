dfae6b349e04fce301225b2386fdda172e704886
Joran Honig <joran.honig@gmail.com>
2018-10-13 21:31:35 +0200

    Change loglevel to debug for some statements

--- mythril/laser/ethereum/svm.py
+++ mythril/laser/ethereum/svm.py
@@ -263,7 +263,7 @@ class LaserEVM:
             environment.active_function_name = disassembly.addr_to_func[address]
             new_node.flags |= NodeFlags.FUNC_ENTRY
 
-            logging.info(
+            logging.debug(
                 "- Entering function " + environment.active_account.contract_name + ":" + new_node.function_name)
         elif address == 0:
             environment.active_function_name = "fallback"
