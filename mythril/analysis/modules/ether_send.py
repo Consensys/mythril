91a6881ed5ca0afa2c30e195b6695ad1c3826855
Joran Honig <joran.honig@gmail.com>
2018-10-17 15:29:44 +0200

    Ether send module to work based on the newer version of laser

--- mythril/analysis/modules/ether_send.py
+++ mythril/analysis/modules/ether_send.py
@@ -1,10 +1,8 @@
-from z3 import *
 from mythril.analysis.ops import *
 from mythril.analysis import solver
 from mythril.analysis.report import Issue
 from mythril.analysis.swc_data import UNPROTECTED_ETHER_WITHDRAWAL
 from mythril.exceptions import UnsatError
-import re
 import logging
 
 
@@ -17,111 +15,50 @@ to that index).
 '''
 
 
-def execute(statespace):
+def execute(state_space):
 
     logging.debug("Executing module: ETHER_SEND")
 
     issues = []
 
-    for call in statespace.calls:
+    for k in state_space.nodes:
+        node = state_space.nodes[k]
 
-        state = call.state
-        address = state.get_current_instruction()['address']
+        for state in node.states:
+            issues += _analyze_state(state, node)
 
-        if "callvalue" in str(call.value):
-            logging.debug("[ETHER_SEND] Skipping refund function")
-            continue
+    return issues
 
-        # We're only interested in calls that send Ether
 
-        if call.value.type == VarType.CONCRETE and call.value.val == 0:
-            continue
+def _analyze_state(state, node):
+    issues = []
+    instruction = state.get_current_instruction()
 
-        interesting = False
+    if instruction['opcode'] != "CALL":
+        return []
 
-        description = "In the function `" + call.node.function_name + "` "
+    call_value = state.mstate.stack[-3]
+    target = state.mstate.stack[-2]
 
-        if re.search(r'caller', str(call.to)):
-            description += "a non-zero amount of Ether is sent to msg.sender.\n"
-            interesting = True
+    not_creator_constraints = []
+    if len(state.world_state.transaction_sequence) > 1:
+        creator = state.world_state.transaction_sequence[0].caller
+        for transaction in state.world_state.transaction_sequence[1:]:
+            not_creator_constraints.append(Not(Extract(159, 0, transaction.caller) == Extract(159, 0, creator)))
+            not_creator_constraints.append(Not(Extract(159, 0, transaction.caller) == 0))
 
-        elif re.search(r'calldata', str(call.to)):
-            description += "a non-zero amount of Ether is sent to an address taken from function arguments.\n"
-            interesting = True
+    try:
+        model = solver.get_model(node.constraints + not_creator_constraints + [call_value > 0])
 
-        else:
-            m = re.search(r'storage_([a-z0-9_&^]+)', str(call.to))
+        debug = "SOLVER OUTPUT:\n" + solver.pretty_print_model(model)
 
-            if m:
-                idx = m.group(1)
-
-                description += "a non-zero amount of Ether is sent to an address taken from storage slot " + str(idx) + ".\n"
-
-                func = statespace.find_storage_write(state.environment.active_account.address, idx)
-
-                if func:
-                    description += "There is a check on storage index " + str(idx) + ". This storage slot can be written to by calling the function `" + func + "`.\n"
-                    interesting = True
-                else:
-                    logging.debug("[ETHER_SEND] No storage writes to index " + str(idx))
-
-        if interesting:
-
-            node = call.node
-
-            can_solve = True
-            constrained = False
-
-            index = 0
-
-            while can_solve and index < len(node.constraints):
-
-                constraint = node.constraints[index]
-                index += 1
-                logging.debug("[ETHER_SEND] Constraint: " + str(constraint))
-
-                m = re.search(r'storage_([a-z0-9_&^]+)', str(constraint))
-
-                if m:
-
-                    constrained = True
-                    idx = m.group(1)
-
-                    func = statespace.find_storage_write(state.environment.active_account.address, idx)
-
-                    if func:
-                        description += "\nThere is a check on storage index " + str(idx) + ". This storage slot can be written to by calling the function `" + func + "`."
-                    else:
-                        logging.debug("[ETHER_SEND] No storage writes to index " + str(idx))
-                        can_solve = False
-                        break
-
-                # CALLER may also be constrained to hardcoded address. I.e. 'caller' and some integer
-
-                elif re.search(r"caller", str(constraint)) and re.search(r'[0-9]{20}', str(constraint)):
-                    constrained = True
-                    can_solve = False
-                    break
-
-            if not constrained:
-                description += "It seems that this function can be called without restrictions."
-
-            if can_solve:
-
-                try:
-                    model = solver.get_model(node.constraints)
-
-                    for d in model.decls():
-                        logging.debug("[ETHER_SEND] main model: %s = 0x%x" % (d.name(), model[d].as_long()))
-
-                    debug = "SOLVER OUTPUT:\n" + solver.pretty_print_model(model)
-
-                    issue = Issue(contract=call.node.contract_name, function=call.node.function_name, address=address,
-                                  title="Ether send", _type="Warning", swc_id=UNPROTECTED_ETHER_WITHDRAWAL,
-                                  description=description, debug=debug)
-                    issues.append(issue)
-
-                except UnsatError:
-                    logging.debug("[ETHER_SEND] no model found")
+        issue = Issue(contract=node.contract_name, function=node.function_name, address=instruction['address'],
+                      swc_id=UNPROTECTED_ETHER_WITHDRAWAL, title="Ether send", _type="Warning",
+                      description="It seems that an attacker is able to execute an call instruction,"
+                                  " this can mean that the attacker is able is able to extract funds "
+                                  "out of the contract.".format(target), debug=debug)
+        issues.append(issue)
+    except UnsatError:
+        logging.debug("[UNCHECKED_SUICIDE] no model found")
 
     return issues
