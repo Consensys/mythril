725d6c61d5c9a0564e7b350db566aeac38480416
Nathan <nbanmp@gmail.com>
2018-10-25 18:40:24 -0400

    Fix calldata constraints being overwritten

--- mythril/laser/ethereum/transaction/symbolic.py
+++ mythril/laser/ethereum/transaction/symbolic.py
@@ -89,7 +89,7 @@ def _setup_global_state_for_execution(laser_evm, transaction):
             )
         )
 
-        global_state.mstate.constraints = transaction.world_state.node.constraints
+        global_state.mstate.constraints += transaction.world_state.node.constraints
         new_node.constraints = global_state.mstate.constraints
 
     global_state.world_state.transaction_sequence.append(transaction)
