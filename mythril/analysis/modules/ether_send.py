ef8a7e74a91163465f78ef2cbfcddce99ffb7bec
Joran Honig <joran.honig@gmail.com>
2018-10-26 20:16:44 +0200

    black

--- mythril/analysis/modules/ether_send.py
+++ mythril/analysis/modules/ether_send.py
@@ -6,13 +6,13 @@ from mythril.exceptions import UnsatError
 import logging
 
 
-'''
+"""
 MODULE DESCRIPTION:
 
 Check for CALLs that send >0 Ether to either the transaction sender, or to an address provided as a function argument.
 If msg.sender is checked against a value in storage, check whether that storage index is tainted (i.e. there's an unconstrained write
 to that index).
-'''
+"""
 
 
 def execute(state_space):
@@ -34,7 +34,7 @@ def _analyze_state(state, node):
     issues = []
     instruction = state.get_current_instruction()
 
-    if instruction['opcode'] != "CALL":
+    if instruction["opcode"] != "CALL":
         return []
 
     call_value = state.mstate.stack[-3]
@@ -44,19 +44,32 @@ def _analyze_state(state, node):
     if len(state.world_state.transaction_sequence) > 1:
         creator = state.world_state.transaction_sequence[0].caller
         for transaction in state.world_state.transaction_sequence[1:]:
-            not_creator_constraints.append(Not(Extract(159, 0, transaction.caller) == Extract(159, 0, creator)))
-            not_creator_constraints.append(Not(Extract(159, 0, transaction.caller) == 0))
+            not_creator_constraints.append(
+                Not(Extract(159, 0, transaction.caller) == Extract(159, 0, creator))
+            )
+            not_creator_constraints.append(
+                Not(Extract(159, 0, transaction.caller) == 0)
+            )
 
     try:
-        model = solver.get_model(node.constraints + not_creator_constraints + [call_value > 0])
+        model = solver.get_model(
+            node.constraints + not_creator_constraints + [call_value > 0]
+        )
 
         debug = "SOLVER OUTPUT:\n" + solver.pretty_print_model(model)
 
-        issue = Issue(contract=node.contract_name, function=node.function_name, address=instruction['address'],
-                      swc_id=UNPROTECTED_ETHER_WITHDRAWAL, title="Ether send", _type="Warning",
-                      description="It seems that an attacker is able to execute an call instruction,"
-                                  " this can mean that the attacker is able to extract funds "
-                                  "out of the contract.".format(target), debug=debug)
+        issue = Issue(
+            contract=node.contract_name,
+            function=node.function_name,
+            address=instruction["address"],
+            swc_id=UNPROTECTED_ETHER_WITHDRAWAL,
+            title="Ether send",
+            _type="Warning",
+            description="It seems that an attacker is able to execute an call instruction,"
+            " this can mean that the attacker is able to extract funds "
+            "out of the contract.".format(target),
+            debug=debug,
+        )
         issues.append(issue)
     except UnsatError:
         logging.debug("[UNCHECKED_SUICIDE] no model found")
