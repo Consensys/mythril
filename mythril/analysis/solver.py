4f00ae5bddb50aad74269e16c84d172bb9849369
Nathan <nbanmp@gmail.com>
2018-10-28 08:52:21 -0400

    solver optimizes calldatasize

--- mythril/analysis/solver.py
+++ mythril/analysis/solver.py
@@ -1,4 +1,4 @@
-from z3 import Solver, simplify, sat, unknown, FuncInterp, Extract, UGE
+from z3 import simplify, sat, unknown, FuncInterp, Extract, UGE, Optimize
 from mythril.exceptions import UnsatError
 from mythril.laser.ethereum.transaction.transaction_models import (
     ContractCreationTransaction,
@@ -6,12 +6,17 @@ from mythril.laser.ethereum.transaction.transaction_models import (
 import logging
 
 
-def get_model(constraints):
-    s = Solver()
+def get_model(constraints, maximize=(), minimize=()):
+    s = Optimize()
     s.set("timeout", 100000)
 
     for constraint in constraints:
         s.add(constraint)
+    for objective in maximize:
+        s.maximize(objective)
+    for objective in minimize:
+        s.minimize(objective)
+
     result = s.check()
     if result == sat:
         return s.model()
@@ -63,6 +68,7 @@ def get_transaction_sequence(
     txs = {}
     creation_tx_ids = []
     tx_constraints = constraints.copy()
+    minimize = []
 
     for transaction in transaction_sequence:
         tx_id = str(transaction.id)
@@ -84,12 +90,13 @@ def get_transaction_sequence(
                     UGE(max_calldatasize, transaction.call_data.calldatasize)
                 )
 
+            minimize.append(transaction.call_data.calldatasize)
             txs[tx_id] = tx_template.copy()
 
         else:
             creation_tx_ids.append(tx_id)
 
-    model = get_model(tx_constraints)
+    model = get_model(tx_constraints, minimize=minimize)
 
     for transaction in transaction_sequence:
         if not isinstance(transaction, ContractCreationTransaction):
