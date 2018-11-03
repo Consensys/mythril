4cdef20ba470e94fb9349e046180d1d00d829ede
Dominik Muhs <dmuhs@protonmail.ch>
2018-11-03 14:14:20 +0100

    Add transaction to codecopy test

--- tests/instructions/codecopy_test.py
+++ tests/instructions/codecopy_test.py
@@ -1,6 +1,13 @@
 from mythril.disassembler.disassembly import Disassembly
-from mythril.laser.ethereum.state import MachineState, GlobalState, Environment, Account
+from mythril.laser.ethereum.state import (
+    MachineState,
+    GlobalState,
+    WorldState,
+    Environment,
+    Account,
+)
 from mythril.laser.ethereum.instructions import Instruction
+from mythril.laser.ethereum.transaction.transaction_models import MessageCallTransaction
 
 
 def test_codecopy_concrete():
@@ -8,6 +15,9 @@ def test_codecopy_concrete():
     active_account = Account("0x0", code=Disassembly("60606040"))
     environment = Environment(active_account, None, None, None, None, None)
     og_state = GlobalState(None, environment, None, MachineState(gas_limit=8000000))
+    og_state.transaction_stack.append(
+        (MessageCallTransaction(world_state=WorldState(), gas_limit=8000000), None)
+    )
 
     og_state.mstate.stack = [2, 2, 2]
     instruction = Instruction("codecopy", dynamic_loader=None)
