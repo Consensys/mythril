6a7cfcf11fa7313342f04a4cab4a67c3dd4fd07a
Dominik Muhs <dmuhs@protonmail.ch>
2018-11-08 20:09:21 +0100

    Add base analysis module class

---
+++ mythril/analysis/modules/base.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class DetectionModule:
+    def __init__(self, name: str, swc_id: str, hooks: List[str], description: str):
+        self.name = name
+        self.swc_id = swc_id
+        self.hooks = hooks
+        self.description = description
+
+    def execute(self):
+        raise NotImplementedError()
+
+    def __repr__(self):
+        return (
+            "<"
+            "DetectionModule "
+            "name={0.name} "
+            "swc_id={0.swc_id} "
+            "hooks={0.hooks} "
+            "description={0.description}"
+            ">"
+        ).format(self)
