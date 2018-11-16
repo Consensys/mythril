1c02e642f8bb0475c7280f170ea58bbd9f79ba9c
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-11-16 11:14:17 +0700

    Deepcopy account state before deletion on selfdestuct

--- mythril/laser/ethereum/instructions.py
+++ mythril/laser/ethereum/instructions.py
@@ -1194,6 +1194,13 @@ class Instruction:
                 )
                 account_created = True
 
+        global_state.environment.active_account = deepcopy(
+            global_state.environment.active_account
+        )
+        global_state.accounts[
+            global_state.environment.active_account.address
+        ] = global_state.environment.active_account
+
         global_state.environment.active_account.balance = 0
         global_state.environment.active_account.deleted = True
         global_state.current_transaction.end(global_state)
