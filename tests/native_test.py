622a1d63e6bf06624076f8b0c1745910fd8641ae
Dominik Muhs <dmuhs@protonmail.ch>
2018-11-24 19:38:42 +0100

    Fix native tests

--- tests/native_test.py
+++ tests/native_test.py
@@ -5,48 +5,26 @@ from mythril.laser.ethereum.state.global_state import GlobalState
 from mythril.laser.ethereum import svm
 from tests import *
 
+SHA256_TEST = [(0, False) for _ in range(4)]
+RIPEMD160_TEST = [(0, False) for _ in range(2)]
+ECRECOVER_TEST = [(0, False) for _ in range(2)]
+IDENTITY_TEST = [(0, False) for _ in range(2)]
 
-SHA256_TEST = [(0, False) for _ in range(6)]
+# These are Random numbers to check whether the 'if condition' is entered or not
+# (True means entered)
+SHA256_TEST[0] = (hex(5555555555555555), True)
+SHA256_TEST[1] = (hex(323232325445454546), True)
+SHA256_TEST[2] = (hex(34756834765834658), True)
+SHA256_TEST[3] = (hex(8756476956956795876987), True)
 
-RIPEMD160_TEST = [(0, False) for _ in range(6)]
+RIPEMD160_TEST[0] = (hex(999999999999999999993), True)
+RIPEMD160_TEST[1] = (hex(1111111111112), True)
 
-ECRECOVER_TEST = [(0, False) for _ in range(9)]
+ECRECOVER_TEST[0] = (hex(674837568743979857398564869), True)
+ECRECOVER_TEST[1] = (hex(3487683476979311), False)
 
-IDENTITY_TEST = [(0, False) for _ in range(4)]
-
-SHA256_TEST[0] = (
-    5555555555555555,
-    True,
-)  # These are Random numbers to check whether the 'if condition' is entered or not(True means entered)
-SHA256_TEST[1] = (323232325445454546, True)
-SHA256_TEST[2] = (34756834765834658, False)
-SHA256_TEST[3] = (8756476956956795876987, True)
-SHA256_TEST[4] = (5763467587689578369, True)
-SHA256_TEST[5] = (948365957658767467857, False)
-
-RIPEMD160_TEST[0] = (1242435356364, True)
-RIPEMD160_TEST[1] = (6732648654386435, True)
-RIPEMD160_TEST[2] = (97457657536546465, False)
-RIPEMD160_TEST[3] = (56436346436456546, True)
-RIPEMD160_TEST[4] = (999999999999999999993, True)
-RIPEMD160_TEST[5] = (1111111111112, False)
-
-
-ECRECOVER_TEST[0] = (786428768768632537676, True)
-ECRECOVER_TEST[1] = (4897983476979346779638, False)
-ECRECOVER_TEST[2] = (674837568743979857398564869, True)
-ECRECOVER_TEST[3] = (3487683476979311, False)
-ECRECOVER_TEST[4] = (853729594875984769847369, True)
-ECRECOVER_TEST[5] = (83579382475972439587, False)
-ECRECOVER_TEST[6] = (8437589437695876985769, True)
-ECRECOVER_TEST[7] = (9486794873598347697596, False)
-ECRECOVER_TEST[8] = (346934876983476, True)
-
-IDENTITY_TEST[0] = (87426857369875698, True)
-IDENTITY_TEST[1] = (476934798798347, False)
-
-IDENTITY_TEST[2] = (7346948379483769, True)
-IDENTITY_TEST[3] = (83269476937987, False)
+IDENTITY_TEST[0] = (hex(87426857369875698), True)
+IDENTITY_TEST[1] = (hex(476934798798347), False)
 
 
 def _all_info(laser):
@@ -98,10 +76,10 @@ def _all_info(laser):
 def _test_natives(laser_info, test_list, test_name):
     success = 0
     for i, j in test_list:
-        if (str(i) in laser_info) == j:
+        if (str(i) in laser_info or str(int(i, 16)) in laser_info) == j:
             success += 1
         else:
-            print("Failed: " + str(i) + " " + str(j))
+            print("Failed:", str(int(i, 16)), str(j))
     assert success == len(test_list)
 
 
@@ -116,8 +94,8 @@ class NativeTests(BaseTestCase):
         laser.sym_exec(account.address)
 
         laser_info = str(_all_info(laser))
-        print("\n")
 
+        print(laser_info)
         _test_natives(laser_info, SHA256_TEST, "SHA256")
         _test_natives(laser_info, RIPEMD160_TEST, "RIPEMD160")
         _test_natives(laser_info, ECRECOVER_TEST, "ECRECOVER")
