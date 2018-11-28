91617c25b9418f2ced7dda9acb460e1b75f47f93
Joran Honig <joran.honig@gmail.com>
2018-11-28 14:35:37 +0100

    mythril/laser/ethereum/state/calldata: Add documentation and type hints

--- mythril/laser/ethereum/state/calldata.py
+++ mythril/laser/ethereum/state/calldata.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Union, Any
 from z3 import BitVecVal, BitVecRef, BitVec, simplify, Concat, If, ExprRef
-from z3.z3types import Z3Exception
+from z3.z3types import Z3Exception, Model
 
 from mythril.laser.ethereum.util import get_concrete_int
 
@@ -12,18 +12,26 @@ class CalldataType(Enum):
 
 
 class BaseCalldata:
+    """
+    Base calldata class
+    This represents the calldata provided when sending a transaction to a contract
+    """
     def __init__(self, tx_id):
         self.tx_id = tx_id
 
     @property
-    def calldatasize(self):
+    def calldatasize(self) -> ExprRef:
+        """
+        :return: Calldata size for this calldata object
+        """
         result = self.size
         if isinstance(result, int):
             return BitVecVal(result, 256)
         return result
 
-    def get_word_at(self, index: int):
-        return self[index : index + 32]
+    def get_word_at(self, offset: int) -> ExprRef:
+        """ Gets word at offset"""
+        return self[offset: offset + 32]
 
     def __getitem__(self, item: Union[int, slice]) -> Any:
         if isinstance(item, int):
@@ -49,19 +57,26 @@ class BaseCalldata:
 
         raise ValueError
 
-    def _load(self, item: Union[int, ExprRef]):
+    def _load(self, item: Union[int, ExprRef]) -> Any:
         raise NotImplementedError()
 
     @property
-    def size(self):
+    def size(self) -> Union[ExprRef, int]:
+        """ Returns the exact size of this calldata, this is not normalized"""
         raise NotImplementedError()
 
-    def concrete(self, model):
+    def concrete(self, model: Model) -> list:
+        """ Returns a concrete version of the calldata using the provided model"""
         raise NotImplementedError
 
 
 class ConcreteCalldata(BaseCalldata):
-    def __init__(self, tx_id, calldata):
+    def __init__(self, tx_id: int, calldata: list):
+        """
+        Initializes the ConcreteCalldata object
+        :param tx_id: Id of the transaction that the calldata is for.
+        :param calldata: The concrete calldata content
+        """
         self._calldata = calldata
         super().__init__(tx_id)
 
@@ -77,16 +92,20 @@ class ConcreteCalldata(BaseCalldata):
             value = If(item == i, self._calldata[i], value)
         return value
 
-    def concrete(self, model):
+    def concrete(self, model: Model) -> list:
         return self._calldata
 
     @property
-    def size(self):
+    def size(self) -> int:
         return len(self._calldata)
 
 
 class SymbolicCalldata(BaseCalldata):
-    def __init__(self, tx_id):
+    def __init__(self, tx_id: int):
+        """
+        Initializes the SymbolicCalldata object
+        :param tx_id: Id of the transaction that the calldata is for.
+        """
         self._reads = []
         self._size = BitVec("calldatasize", 256)
         super().__init__(tx_id)
@@ -94,22 +113,21 @@ class SymbolicCalldata(BaseCalldata):
     def _load(self, item: Union[int, ExprRef], clean=False) -> Any:
         x = BitVecVal(item, 256) if isinstance(item, int) else item
 
-        b = BitVec("{}_calldata_{}".format(self.tx_id, str(item)), 8)
-        c = x > self._size
-        a = BitVecVal(0, 8)
-        symbolic_base_value = If(c, a, b)
+        symbolic_base_value = If(
+            x > self._size,
+            BitVecVal(0, 8),
+            BitVec("{}_calldata_{}".format(self.tx_id, str(item)), 8)
+        )
 
         return_value = symbolic_base_value
         for r_index, r_value in self._reads:
             return_value = If(r_index == item, r_value, return_value)
 
-        return_value = simplify(return_value)
-
         if not clean:
             self._reads.append((item, symbolic_base_value))
-        return return_value
+        return simplify(return_value)
 
-    def concrete(self, model):
+    def concrete(self, model: Model) -> list:
         concrete_length = get_concrete_int(model.eval(self.size, model_completion=True))
         result = []
         for i in range(concrete_length):
@@ -120,5 +138,5 @@ class SymbolicCalldata(BaseCalldata):
         return result
 
     @property
-    def size(self):
+    def size(self) -> ExprRef:
         return self._size
