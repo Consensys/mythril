30746d95d9cae1821492955cf33a2df1a5f9cdec
Nikhil Parasaram <tommycjniko@gmail.com>
2019-01-01 17:42:07 +0530

    Add the node check in concolic.py

--- mythril/laser/ethereum/transaction/concolic.py
+++ mythril/laser/ethereum/transaction/concolic.py
@@ -56,8 +56,9 @@ def _setup_global_state_for_execution(laser_evm, transaction) -> None:
 
     new_node = Node(global_state.environment.active_account.contract_name)
 
-    laser_evm.nodes[new_node.uid] = new_node
-    if transaction.world_state.node:
+    if laser_evm.requires_statespace:
+        laser_evm.nodes[new_node.uid] = new_node
+    if transaction.world_state.node and laser_evm.requires_statespace:
         laser_evm.edges.append(
             Edge(
                 transaction.world_state.node.uid,
@@ -66,6 +67,7 @@ def _setup_global_state_for_execution(laser_evm, transaction) -> None:
                 condition=None,
             )
         )
+
     global_state.node = new_node
     new_node.states.append(global_state)
     laser_evm.work_list.append(global_state)
