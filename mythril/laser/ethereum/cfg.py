b57017832c3184027a42aeb59d514116df12cec6
Nikhil Parasaram <tommycjniko@gmail.com>
2019-01-17 16:35:19 +0530

    Fix all svm errors in mypy

--- mythril/laser/ethereum/cfg.py
+++ mythril/laser/ethereum/cfg.py
@@ -23,6 +23,9 @@ class JumpType(Enum):
 class NodeFlags(Flags):
     """A collection of flags to denote the type a call graph node can have."""
 
+    def __or__(self, other) -> "NodeFlags":
+        return super().__or__(self, other)
+
     FUNC_ENTRY = 1
     CALL_RETURN = 2
 
