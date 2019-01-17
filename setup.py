5ae1052bdf3cad3c1814875f110d7482faf72115
Nikhil Parasaram <tommycjniko@gmail.com>
2019-01-17 13:40:20 +0530

    reformat setup.py with black

--- setup.py
+++ setup.py
@@ -98,13 +98,7 @@ setup(
         "persistent>=4.2.0",
         "ethereum-input-decoder>=0.2.2",
     ],
-
-    tests_require=[
-        'mypy',
-        'pytest>=3.6.0',
-        'pytest_mock',
-        'pytest-cov'
-    ],
+    tests_require=["mypy", "pytest>=3.6.0", "pytest_mock", "pytest-cov"],
     python_requires=">=3.5",
     extras_require={},
     package_data={"mythril.analysis.templates": ["*"], "": ["signatures.db"]},
