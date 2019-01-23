39f7ecf56668632ea7f1049fc249ff73f26922b5
Joran Honig <joran.honig@gmail.com>
2019-01-24 00:22:42 +0100

    add initial independence solver object to smt

--- mythril/laser/smt/independence_solver.py
+++ mythril/laser/smt/independence_solver.py
@@ -1,20 +1,14 @@
-from z3 import BitVec, AstRef, ExprRef, BitVecVal, BitVecNumRef
-
-a = BitVec('a', 256)
-c = BitVec('c', 256)
-b = c * (a + 1)
-
-e = BitVec('d', 256)
+import z3
 
 
-def get_expr_variables(expression: ExprRef):
+def get_expr_variables(expression: z3.ExprRef):
     """
     Get's the variables that make up the current expression
     :param expression:
     :return:
     """
     result = []
-    if not expression.children() and not isinstance(expression, BitVecNumRef):
+    if not expression.children() and not isinstance(expression, z3.BitVecNumRef):
         result.append(expression)
     for child in expression.children():
         c_children = get_expr_variables(child)
@@ -66,10 +60,93 @@ class DependenceMap:
 
         return new_bucket
 
-dmap = DependenceMap()
 
-dmap.add_condition(a)
-dmap.add_condition(b)
-dmap.add_condition(e)
+class IndependenceSolver:
+    """An SMT solver object."""
 
-print("yeah")
+    def __init__(self):
+        """"""
+        self.raw = z3.Solver()
+        self.constraints = []
+        self.models = []
+
+    def set_timeout(self, timeout: int) -> None:
+        """Sets the timeout that will be used by this solver, timeout is in
+        milliseconds.
+
+        :param timeout:
+        """
+        self.raw.set(timeout=timeout)
+
+    def add(self, constraints: list) -> None:
+        """Adds the constraints to this solver.
+
+        :param constraints:
+        :return:
+        """
+        constraints = [c.raw for c in constraints]
+        self.constraints.extend(constraints)
+
+    def append(self, constraints: list) -> None:
+        """Adds the constraints to this solver.
+
+        :param constraints:
+        :return:
+        """
+        constraints = [c.raw for c in constraints]
+        self.constraints.extend(constraints)
+
+    def check(self):
+        """Returns z3 smt check result.
+
+        :return:
+        """
+        dependence_map = DependenceMap()
+        for constraint in self.constraints:
+            dependence_map.add_condition(constraint)
+
+        self.models = []
+        for bucket in dependence_map.buckets:
+            self.raw.reset()
+            self.raw.append(*bucket.expressions)
+            check_result = self.raw.check()
+            if check_result == z3.unsat:
+                return z3.unsat
+            elif check_result == z3.unknown:
+                return z3.unknown
+            else:
+                self.models.append(self.raw.model())
+
+        return z3.sat
+
+    def model(self):
+        """Returns z3 model for a solution.
+
+        :return:
+        """
+        return self.models
+
+    def reset(self) -> None:
+        """Reset this solver."""
+        self.constraints = []
+
+    def pop(self, num) -> None:
+        """Pop num constraints from this solver.
+
+        :param num:
+        """
+        self.constraints.pop(num)
+
+
+from mythril.laser.smt import symbol_factory
+a = symbol_factory.BitVecSym('a', 256)
+c = symbol_factory.BitVecSym('c', 256)
+b = c == (a + symbol_factory.BitVecVal(1, 256))
+
+e = symbol_factory.BitVecSym('d', 256) == symbol_factory.BitVecVal(3, 256)
+
+solver = IndependenceSolver()
+solver.add([b, e])
+result = solver.check()
+
+print("hello")
