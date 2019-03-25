f0ce805a7c1fd5197f5575b5868788066429f3bd
Nikhil Parasaram <tommycjniko@gmail.com>
2019-03-25 20:05:35 +0530

    Change the field to integer

--- mythril/analysis/report.py
+++ mythril/analysis/report.py
@@ -231,11 +231,7 @@ class Report:
                     },
                     "severity": issue.severity,
                     "locations": [{"sourceMap": "%d:1:%d" % (issue.address, idx)}],
-                    "extra": {
-                        "discoveryTime": "{}".format(
-                            int(issue.discovery_time * 10 ** 9)
-                        )
-                    },
+                    "extra": {"discoveryTime": int(issue.discovery_time * 10 ** 9)},
                 }
             )
         meta_data = self._get_exception_data()
