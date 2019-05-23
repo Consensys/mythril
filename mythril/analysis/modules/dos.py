73500d86b6d39e818aa6899d2b4dda48e1e8d299
Bernhard Mueller <bernhard.mueller11@gmail.com>
2019-05-23 20:01:52 +0200

    Norhh feedback #3

--- mythril/analysis/modules/dos.py
+++ mythril/analysis/modules/dos.py
@@ -1,4 +1,4 @@
-"""This module contains the detection code for insecure delegate call usage."""
+"""This module contains the detection code SWC-128 - DOS with block gas limit."""
 
 import logging
 from typing import Dict, cast, List
@@ -14,12 +14,21 @@ log = logging.getLogger(__name__)
 
 
 class LoopAnnotation(StateAnnotation):
-    def __init__(self, jump_address: int) -> None:
-        self.jump_address = jump_address
+    def __init__(self, loop_start: int, loop_end: int) -> None:
+        self.loop_start = loop_start
+        self.loop_end = loop_end
+
+    def contains(self, address: int) -> bool:
+        return self.loop_start < address < self.loop_end
 
 
 class DOS(DetectionModule):
-    """This module detects denial of service by exhausting gas."""
+    """This module consists of a makeshift loop detector that annotates the state with
+    a list of byte ranges likely to be loops. If a CALL or SSTORE detection is found in
+    one of the ranges it creates a low-severity issue. This is not super precise but
+    good enough to identify places that warrant a closer look. Checking the loop condition
+    would be a possible improvement.
+     """
 
     def __init__(self) -> None:
         """"""
@@ -51,6 +60,7 @@ class DOS(DetectionModule):
         """
 
         opcode = state.get_current_instruction()["opcode"]
+        address = state.get_current_instruction()["address"]
 
         if opcode == "JUMPI":
 
@@ -62,9 +72,14 @@ class DOS(DetectionModule):
                 try:
                     self._jumpdest_count[transaction][target] += 1
                     if self._jumpdest_count[transaction][target] == 4:
-                        state.annotate(
-                            LoopAnnotation(state.get_current_instruction()["address"])
+
+                        annotation = (
+                            LoopAnnotation(address, target)
+                            if target > address
+                            else LoopAnnotation(target, address)
                         )
+
+                        state.annotate(annotation)
                 except KeyError:
                     self._jumpdest_count[transaction][target] = 0
 
@@ -78,30 +93,34 @@ class DOS(DetectionModule):
                 List[LoopAnnotation], list(state.get_annotations(LoopAnnotation))
             )
 
-            if len(annotations) > 0:
+            for annotation in annotations:
 
-                operation = (
-                    "A storage modification" if opcode == "CALL" else "An external call"
-                )
+                if annotation.contains(address):
 
-                description_head = (
-                    "Potential Denial-of-Service if block gas limit is reached."
-                )
-                description_tail = "{} is executed in a loop.".format(operation)
+                    operation = (
+                        "A storage modification"
+                        if opcode == "SSTORE"
+                        else "An external call"
+                    )
 
-                issue = Issue(
-                    contract=state.environment.active_account.contract_name,
-                    function_name=state.environment.active_function_name,
-                    address=annotations[0].jump_address,
-                    swc_id=DOS_WITH_BLOCK_GAS_LIMIT,
-                    bytecode=state.environment.code.bytecode,
-                    title="Potential Denial-of-Service if block gas limit is reached",
-                    severity="Low",
-                    description_head=description_head,
-                    description_tail=description_tail,
-                    gas_used=(state.mstate.min_gas_used, state.mstate.max_gas_used),
-                )
-                return [issue]
+                    description_head = (
+                        "Potential denial-of-service if block gas limit is reached."
+                    )
+                    description_tail = "{} is executed in a loop.".format(operation)
+
+                    issue = Issue(
+                        contract=state.environment.active_account.contract_name,
+                        function_name=state.environment.active_function_name,
+                        address=annotation.loop_start,
+                        swc_id=DOS_WITH_BLOCK_GAS_LIMIT,
+                        bytecode=state.environment.code.bytecode,
+                        title="Potential denial-of-service if block gas limit is reached",
+                        severity="Low",
+                        description_head=description_head,
+                        description_tail=description_tail,
+                        gas_used=(state.mstate.min_gas_used, state.mstate.max_gas_used),
+                    )
+                    return [issue]
 
         return []
 
