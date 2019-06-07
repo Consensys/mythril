89d7d971c2c4b158d26150138ff24801d5118a7b
Bernhard Mueller <bernhard.mueller11@gmail.com>
2019-06-08 00:59:15 +0200

    Update values

--- mythril/support/dynamic_timeout.py
+++ mythril/support/dynamic_timeout.py
@@ -10,8 +10,8 @@ def get_timeout(bytecode_length: int, mode: str) -> int:
     :param mode: Preset mode (fast or deep)
     """
     if mode == "fast":
-        return int(min(120, 15 * exp(bytecode_length / 6000)))
+        return int(min(100, 15 * exp(bytecode_length / 6000)))
     elif mode == "deep":
-        return int(min(3600, 30 * exp(bytecode_length / 3000)))
+        return int(min(1800, 30 * exp(bytecode_length / 3000)))
 
     raise UnsupportedModeError
