c7a3edfa48aeb77fdad12bb2c7445c349d6b5793
Nikhil Parasaram <tommycjniko@gmail.com>
2019-06-12 11:41:51 +0530

    Reformat with black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -132,13 +132,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (
-        master_doc,
-        "Mythril.tex",
-        "Mythril Documentation",
-        "Bernhard Mueller",
-        "manual",
-    )
+    (master_doc, "Mythril.tex", "Mythril Documentation", "Bernhard Mueller", "manual")
 ]
 
 
@@ -146,9 +140,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, "mythril", "Mythril Documentation", [author], 1)
-]
+man_pages = [(master_doc, "mythril", "Mythril Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
