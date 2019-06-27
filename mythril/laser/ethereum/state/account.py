584b7c88b80155b08d01b5e5cfdf133c3b239c53
Nikhil Parasaram <tommycjniko@gmail.com>
2019-06-27 12:50:34 +0530

    Use proper keccak inputs for maps

--- mythril/laser/ethereum/state/account.py
+++ mythril/laser/ethereum/state/account.py
@@ -3,10 +3,19 @@
 This includes classes representing accounts and their storage.
 """
 from copy import copy, deepcopy
-from typing import Any, Dict, Union
+from typing import Any, Dict, Union, Tuple, cast
 
 
-from mythril.laser.smt import Array, K, BitVec, simplify, BitVecFunc, Extract, BaseArray
+from mythril.laser.smt import (
+    Array,
+    K,
+    BitVec,
+    simplify,
+    BitVecFunc,
+    Extract,
+    BaseArray,
+    Concat,
+)
 from mythril.disassembler.disassembly import Disassembly
 from mythril.laser.smt import symbol_factory
 
@@ -24,15 +33,25 @@ class Storage:
         else:
             self._standard_storage = Array("Storage", 256, 256)
         self._map_storage = {}  # type: Dict[BitVec, BaseArray]
-        self._keccak_map_storage = {}  # type: Dict[BitVec, BaseArray]
 
         self.printable_storage = {}  # type: Dict[BitVec, BitVec]
 
         self.dynld = dynamic_loader
         self.address = address
 
-    def __getitem__(self, item: BitVec) -> Any:
-        storage = self._get_corresponding_storage(item)
+    @staticmethod
+    def _sanitize(input_: BitVec) -> BitVec:
+        if input_.size() == 512:
+            return input_
+        if input_.size() > 512:
+            return Extract(511, 0, input_)
+        else:
+            return Concat(symbol_factory.BitVecVal(0, 512 - input_.size()), input_)
+
+    def __getitem__(self, item: BitVec) -> BitVec:
+        storage, is_keccak_storage = self._get_corresponding_storage(item)
+        if is_keccak_storage:
+            item = self._sanitize(cast(BitVecFunc, item).input_)
         value = storage[item]
         if (
             value.value == 0
@@ -70,29 +89,29 @@ class Storage:
         index = Extract(255, 0, key.input_)
         return simplify(index)
 
-    def _get_corresponding_storage(self, key: BitVec) -> BaseArray:
+    def _get_corresponding_storage(self, key: BitVec) -> Tuple[BaseArray, bool]:
         index = self.get_map_index(key)
         if index is None:
             storage = self._standard_storage
-
+            is_keccak_storage = False
         else:
-            if isinstance(key, BitVecFunc) and key.func_name == "keccak256":
-                storage_map = self._keccak_map_storage
-            else:
-                storage_map = self._map_storage
+            storage_map = self._map_storage
             try:
                 storage = storage_map[index]
             except KeyError:
                 if isinstance(self._standard_storage, Array):
-                    self._map_storage[index] = Array("Storage", 256, 256)
+                    storage_map[index] = Array("Storage", 512, 256)
                 else:
-                    self._map_storage[index] = K(256, 256, 0)
-                storage = self._map_storage[index]
-        return storage
+                    storage_map[index] = K(512, 256, 0)
+                storage = storage_map[index]
+            is_keccak_storage = True
+        return storage, is_keccak_storage
 
     def __setitem__(self, key, value: Any) -> None:
-        storage = self._get_corresponding_storage(key)
+        storage, is_keccak_storage = self._get_corresponding_storage(key)
         self.printable_storage[key] = value
+        if is_keccak_storage:
+            key = self._sanitize(key.input_)
         storage[key] = value
 
     def __deepcopy__(self, memodict={}):
