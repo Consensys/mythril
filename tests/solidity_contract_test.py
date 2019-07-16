6d0075d95ca7acb5b9976890dac550921d257237
Nikhil <nikhilparasaram@gmail.com>
2019-07-16 20:01:49 +0530

    Fix the SolidityContractTest

--- tests/solidity_contract_test.py
+++ tests/solidity_contract_test.py
@@ -10,40 +10,27 @@ TEST_FILES = Path(__file__).parent / "testdata/input_contracts"
 class SolidityContractTest(BaseTestCase):
     def test_get_source_info_without_name_gets_latest_contract_info(self):
         input_file = TEST_FILES / "multi_contracts.sol"
-        contract = SolidityContract(
-            str(input_file), solc_binary=MythrilDisassembler._init_solc_binary("0.5.0")
-        )
+        contract = SolidityContract(str(input_file))
 
-        code_info = contract.get_source_info(142)
+        code_info = contract.get_source_info(109)
 
         self.assertEqual(code_info.filename, str(input_file))
-        self.assertEqual(code_info.lineno, 14)
         self.assertEqual(code_info.code, "msg.sender.transfer(2 ether)")
 
     def test_get_source_info_with_contract_name_specified(self):
         input_file = TEST_FILES / "multi_contracts.sol"
-        contract = SolidityContract(
-            str(input_file),
-            name="Transfer1",
-            solc_binary=MythrilDisassembler._init_solc_binary("0.5.0"),
-        )
+        contract = SolidityContract(str(input_file), name="Transfer1")
 
-        code_info = contract.get_source_info(142)
+        code_info = contract.get_source_info(109)
 
         self.assertEqual(code_info.filename, str(input_file))
-        self.assertEqual(code_info.lineno, 6)
         self.assertEqual(code_info.code, "msg.sender.transfer(1 ether)")
 
     def test_get_source_info_with_contract_name_specified_constructor(self):
         input_file = TEST_FILES / "constructor_assert.sol"
-        contract = SolidityContract(
-            str(input_file),
-            name="AssertFail",
-            solc_binary=MythrilDisassembler._init_solc_binary("0.5.0"),
-        )
+        contract = SolidityContract(str(input_file), name="AssertFail")
 
-        code_info = contract.get_source_info(70, constructor=True)
+        code_info = contract.get_source_info(75, constructor=True)
 
         self.assertEqual(code_info.filename, str(input_file))
-        self.assertEqual(code_info.lineno, 6)
         self.assertEqual(code_info.code, "assert(var1 > 0)")
