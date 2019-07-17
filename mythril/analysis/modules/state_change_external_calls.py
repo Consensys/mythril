0b649b65baad16f4d52e18a459f5b7ae6d658058
Nikhil <nikhilparasaram@gmail.com>
2019-07-17 15:59:37 +0530

    Fix the issue in statechange module

--- mythril/analysis/modules/state_change_external_calls.py
+++ mythril/analysis/modules/state_change_external_calls.py
@@ -53,7 +53,7 @@ class StateChangeCallsAnnotation(StateAnnotation):
                 global_state, constraints
             )
         except UnsatError:
-            return None
+            return []
         severity = "Medium" if self.user_defined_address else "Low"
         address = global_state.get_current_instruction()["address"]
         logging.debug(
@@ -67,18 +67,20 @@ class StateChangeCallsAnnotation(StateAnnotation):
             "state change takes place. This can lead to business logic vulnerabilities."
         )
 
-        return Issue(
-            contract=global_state.environment.active_account.contract_name,
-            function_name=global_state.environment.active_function_name,
-            address=address,
-            title="State change after external call",
-            severity=severity,
-            description_head=description_head,
-            description_tail=description_tail,
-            swc_id=REENTRANCY,
-            bytecode=global_state.environment.code.bytecode,
-            transaction_sequence=transaction_sequence,
-        )
+        return [
+            Issue(
+                contract=global_state.environment.active_account.contract_name,
+                function_name=global_state.environment.active_function_name,
+                address=address,
+                title="State change after external call",
+                severity=severity,
+                description_head=description_head,
+                description_tail=description_tail,
+                swc_id=REENTRANCY,
+                bytecode=global_state.environment.code.bytecode,
+                transaction_sequence=transaction_sequence,
+            )
+        ]
 
 
 class StateChange(DetectionModule):
@@ -171,7 +173,7 @@ class StateChange(DetectionModule):
         for annotation in annotations:
             if not annotation.state_change_states:
                 continue
-            vulnerabilities.append(annotation.get_issue(global_state))
+            vulnerabilities += annotation.get_issue(global_state)
         return vulnerabilities
 
     @staticmethod
