9c7e7d02bfea450f83a0d980b8ba582098a61a29
Nikhil <nikhilparasaram@gmail.com>
2019-08-07 21:40:33 +0530

    Enforce concat extract invariance

--- mythril/laser/smt/bitvecfunc.py
+++ mythril/laser/smt/bitvecfunc.py
@@ -125,6 +125,7 @@ class BitVecFunc(BitVec):
         input_: "BitVec" = None,
         annotations: Optional[Annotations] = None,
         nested_functions: Optional[List["BitVecFunc"]] = None,
+        concat_args: List = None,
     ):
         """
 
@@ -138,6 +139,7 @@ class BitVecFunc(BitVec):
         self.input_ = input_
         self.nested_functions = nested_functions or []
         self.nested_functions = list(dict.fromkeys(self.nested_functions))
+        self.concat_args = concat_args or []
         if isinstance(input_, BitVecFunc):
             self.nested_functions.extend(input_.nested_functions)
         super().__init__(raw, annotations)
@@ -292,3 +294,38 @@ class BitVecFunc(BitVec):
 
     def __hash__(self) -> int:
         return self.raw.__hash__()
+
+
+class BitVecFuncExtract(BitVecFunc):
+    """A bit vector function wrapper, useful for preserving Extract() and Concat() operations"""
+
+    def __init__(
+        self,
+        raw: z3.BitVecRef,
+        func_name: Optional[str],
+        input_: "BitVec" = None,
+        annotations: Optional[Annotations] = None,
+        nested_functions: Optional[List["BitVecFunc"]] = None,
+        concat_args: List = None,
+        low=None,
+        high=None,
+        parent=None,
+    ):
+        """
+
+        :param raw: The raw bit vector symbol
+        :param func_name: The function name. e.g. sha3
+        :param input: The input to the functions
+        :param annotations: The annotations the BitVecFunc should start with
+        """
+        super().__init__(
+            raw=raw,
+            func_name=func_name,
+            input_=input_,
+            annotations=annotations,
+            nested_functions=nested_functions,
+            concat_args=concat_args,
+        )
+        self.low = low
+        self.high = high
+        self.parent = parent
---
+++ tests/laser/smt/concat_extract_invariance.py
@@ -0,0 +1,51 @@
+from mythril.laser.smt import Extract, Concat, symbol_factory, BitVecFunc
+
+
+def test_concat_extract_invariance1():
+    input_ = symbol_factory.BitVecSym("input", 256)
+    output_ = symbol_factory.BitVecFuncSym(
+        "Keccak[input]", size=256, func_name="keccak256", input_=input_
+    )
+    p1 = Extract(127, 0, output_)
+    p2 = Extract(255, 128, output_)
+    construct = Concat(p2, p1)
+    assert isinstance(construct, BitVecFunc)
+    assert construct.input_ == input_
+
+
+def test_concat_extract_invariance2():
+    input_ = symbol_factory.BitVecSym("input", 256)
+    output_ = symbol_factory.BitVecFuncSym(
+        "Keccak[input]", size=256, func_name="keccak256", input_=input_
+    )
+    construct = Concat(output_, symbol_factory.BitVecVal(0, 256))
+    assert Extract(511, 256, construct).input_ == input_
+
+
+def test_concat_extract_invariance3():
+    input_ = symbol_factory.BitVecSym("input", 256)
+    output_ = symbol_factory.BitVecFuncSym(
+        "Keccak[input]", size=256, func_name="keccak256", input_=input_
+    )
+    output2_ = symbol_factory.BitVecFuncSym(
+        "Keccak[output_]", size=256, func_name="keccak256", input_=output_
+    )
+
+    construct = Concat(output2_, symbol_factory.BitVecVal(0, 256))
+    assert Extract(511, 256, construct).input_.input_ == input_
+
+
+def test_concat_extract_invariance4():
+    input_ = symbol_factory.BitVecSym("input", 256)
+    output_ = symbol_factory.BitVecFuncSym(
+        "Keccak[input]", size=256, func_name="keccak256", input_=input_
+    )
+    output2_ = symbol_factory.BitVecFuncSym(
+        "Keccak[output_]",
+        size=256,
+        func_name="keccak256",
+        input_=Concat(output_, symbol_factory.BitVecVal(0, 256)),
+    )
+
+    construct = Concat(output2_, symbol_factory.BitVecVal(0, 256))
+    assert Extract(511, 256, Extract(511, 256, construct).input_).input_ == input_
