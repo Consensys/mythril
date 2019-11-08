6df5fa7c7534d0dca42b63c7db4f1abe5728eb34
Nikhil Parasaram <nikhilparasaram@gmail.com>
2019-11-08 14:35:00 +0000

    Ignore mypy error

--- mythril/analysis/modules/dependence_on_predictable_vars.py
+++ mythril/analysis/modules/dependence_on_predictable_vars.py
@@ -193,7 +193,7 @@ class PredictableDependenceModule(DetectionModule):
 
                     # Why the second constraint? Because without it Z3 returns a solution where param overflows.
 
-                    solver.get_model(state.world_state.constraints + constraint)
+                    solver.get_model(state.world_state.constraints + constraint)  # type: ignore
                     state.annotate(OldBlockNumberUsedAnnotation(constraint))
 
                 except UnsatError:
