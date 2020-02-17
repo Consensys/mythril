f37164af0a061d518e2fe088b1d4502f86bfca8e
Joran Honig <joran.honig@gmail.com>
2020-02-17 14:40:42 +0100

    import reset callback modules from module

--- mythril/analysis/module/__init__.py
+++ mythril/analysis/module/__init__.py
@@ -1,3 +1,3 @@
 from mythril.analysis.module.base import EntryPoint, DetectionModule
 from mythril.analysis.module.loader import ModuleLoader
-from mythril.analysis.module.util import get_detection_module_hooks
+from mythril.analysis.module.util import get_detection_module_hooks, reset_callback_modules
