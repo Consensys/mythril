388bd78961c2b0bc3cee70b26a614dc1524fee84
Nikhil Parasaram <nikhilparasaram@gmail.com>
2020-02-29 09:20:28 +0000

    Set dummy infura_id in test

--- tests/mythril/mythril_config_test.py
+++ tests/mythril/mythril_config_test.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 from configparser import ConfigParser
 from pathlib import Path
@@ -13,20 +14,21 @@ def test_config_path_dynloading():
         Path(__file__).parent.parent / "testdata/mythril_config_inputs/config.ini"
     )
     config.set_api_from_config_path()
-    assert "mainnet.infura.io/v3" in config.eth.host
+    assert "mainnet.infura.io/v3/blah" == config.eth.host
 
 
 rpc_types_tests = [
-    ("infura", "mainnet.infura.io/v3", None, True),
+    ("infura", "mainnet.infura.io/v3/blah", None, True),
     ("ganache", "localhost", None, True),
-    ("infura-rinkeby", "rinkeby.infura.io/v3", None, True),
-    ("infura-ropsten", "ropsten.infura.io/v3", None, True),
-    ("infura-kovan", "kovan.infura.io/v3", None, True),
+    ("infura-rinkeby", "rinkeby.infura.io/v3/blah", None, True),
+    ("infura-ropsten", "ropsten.infura.io/v3/blah", None, True),
+    ("infura-kovan", "kovan.infura.io/v3/blah", None, True),
     ("localhost", "localhost", 8545, True),
     ("localhost:9022", "localhost", 9022, True),
     ("pinfura", None, None, False),
     ("infura-finkeby", None, None, False),
 ]
+os.environ["INFURA_ID"] = "blah"
 
 
 @pytest.mark.parametrize("rpc_type,host,port,success", rpc_types_tests)
@@ -34,7 +36,7 @@ def test_set_rpc(rpc_type, host, port, success):
     config = MythrilConfig()
     if success:
         config._set_rpc(rpc_type)
-        assert host in config.eth.host
+        assert host == config.eth.host
     else:
         with pytest.raises(CriticalError):
             config._set_rpc(rpc_type)
