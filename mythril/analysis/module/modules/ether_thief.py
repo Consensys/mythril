417a42909e3b8ef6e037ac9c6af7154438ada4f9
Bernhard Mueller <bernhard.mueller11@gmail.com>
2020-03-08 09:17:04 +0100

    Update description text

--- mythril/analysis/module/modules/ether_thief.py
+++ mythril/analysis/module/modules/ether_thief.py
@@ -84,8 +84,7 @@ class EtherThief(DetectionModule):
             bytecode=state.environment.code.bytecode,
             description_head="Anyone can withdraw ETH from the contract account.",
             description_tail="Arbitrary senders other than the contract creator can withdraw ETH from the contract"
-            + " account without previously having sent an equivalent amount of ETH to it. This is likely to be"
-            + " a vulnerability.",
+            + " account. This is likely to be a vulnerability.",
             detector=self,
             constraints=constraints,
         )
