76a80845d90b5840c4afe85723566369ace495a8
Joran Honig <joran.honig@gmail.com>
2020-03-11 20:12:12 +0100

    refactor loader to use the PluginBuilder interface

--- mythril/laser/plugin/loader.py
+++ mythril/laser/plugin/loader.py
@@ -1,5 +1,7 @@
 from mythril.laser.ethereum.svm import LaserEVM
 from mythril.laser.plugin import LaserPlugin
+from mythril.laser.plugin.builder import PluginBuilder
+
 from typing import Callable, Dict
 
 import logging
@@ -16,32 +18,37 @@ class LaserPluginLoader(object, metaclass=Singleton):
 
     def __init__(self) -> None:
         """ Initializes the plugin loader """
-        self.laser_plugin_builders = {}  # type: Dict[str, Callable[[], LaserPlugin]]
+        self.laser_plugin_builders = {}  # type: Dict[str, PluginBuilder]
 
-    def enable(
-        self, plugin_name: str, plugin_builder: Callable[[], LaserPlugin]
-    ) -> None:
+    def load(self, plugin_builder: PluginBuilder) -> None:
         """ Enables a Laser Plugin
 
-        :param plugin_name: Name of the plugin to enable
-        :param plugin_builder: Function that builds and returns a laser plugin
+        :param plugin_builder: Builder that constructs the plugin
         """
-        log.info(f"Enabling laser plugin: {plugin_name}")
-        if plugin_name in self.laser_plugin_builders:
+        log.info(f"Loading laser plugin: {plugin_builder.plugin_name}")
+        if plugin_builder.plugin_name in self.laser_plugin_builders:
             log.warning(
-                f"Laser plugin with name {plugin_name} was already enabled, skipping..."
+                f"Laser plugin with name {plugin_builder.plugin_name} was already loaded, skipping..."
             )
             return
-        self.laser_plugin_builders[plugin_name] = plugin_builder
+        self.laser_plugin_builders[plugin_builder.plugin_name] = plugin_builder
 
     def is_enabled(self, plugin_name: str) -> bool:
         """ Returns whether the plugin is loaded in the symbolic_vm
 
         :param plugin_name: Name of the plugin to check
         """
-        return plugin_name in self.laser_plugin_builders
+        if not plugin_name in self.laser_plugin_builders:
+            return False
+        else:
+            return self.laser_plugin_builders[plugin_name].enabled
 
-    def load_plugins(self, symbolic_vm: LaserEVM):
+    def enable(self, plugin_name: str):
+        if plugin_name not in self.laser_plugin_builders:
+            return ValueError(f"Plugin with name: {plugin_name} was not loaded")
+        self.laser_plugin_builders[plugin_name].enabled = True
+
+    def instrument_virtual_machine(self, symbolic_vm: LaserEVM):
         """ Load enabled plugins into the passed symbolic virtual machine """
         for plugin_name, plugin_builder in self.laser_plugin_builders:
             log.info(f"Instrumenting symbolic vm with plugin: {plugin_name}")
