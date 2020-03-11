4156c6548e9e9eb52f81c607231bd9140a3d56ff
Joran Honig <joran.honig@gmail.com>
2020-03-11 20:54:11 +0100

    fix import typo

--- mythril/laser/plugin/plugins/__init__.py
+++ mythril/laser/plugin/plugins/__init__.py
@@ -8,4 +8,4 @@ This module contains the implementation of some features
 from mythril.laser.plugin.plugins.benchmark import BenchmarkPluginBuilder
 from mythril.laser.plugin.plugins.coverage.coverage_plugin import CoveragePluginBuilder
 from mythril.laser.plugin.plugins.dependency_pruner import DependencyPrunerBuilder
-from mythril.laser.plugin.plugins.mutation_pruner import MutationPruner
+from mythril.laser.plugin.plugins.mutation_pruner import MutationPrunerBuilder
