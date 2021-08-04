2ffbdb98d5e2420431abb6d040e0771ddd6138a2
norhh <nikhilparasaram@gmail.com>
2021-08-04 23:35:56 +0530

    Add missing files

--- tests/integration_tests/test_solc_settings.py
+++ tests/integration_tests/test_solc_settings.py
@@ -13,7 +13,7 @@ def test_positive_solc_settings():
     json_file_path = str(TESTDATA / "json_test_dir" / "test_file.json")
     file_path = file_dir + "/input_file.sol"
 
-    command = f"cd {file_dir} && python3 {MYTH} analyze {file_path} --solc-json {json_file_path}"
+    command = f"cd {file_dir} && python3 {MYTH} analyze {file_path} --solc-json {json_file_path} --solv 0.8.0"
     output = check_output(command, shell=True, stderr=STDOUT).decode("UTF-8")
     assert "The analysis was completed successfully" in output
 
@@ -21,7 +21,7 @@ def test_positive_solc_settings():
 def test_negative_solc_settings():
     file_path = str(TESTDATA / "json_test_dir" / "dir_a" / "input_file.sol")
 
-    command = f"python3 {MYTH} analyze {file_path}"
+    command = f"python3 {MYTH} analyze {file_path} --solv 0.8.0"
     output = check_output(command, shell=True, stderr=STDOUT).decode("UTF-8")
     assert (
         """ParserError: Source "@openzeppelin/contracts/token/PRC20/PRC20.sol"""
