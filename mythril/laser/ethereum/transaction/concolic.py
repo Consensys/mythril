ec34453d12756161d97f8be797b32716f10bcb16
norhh <nikhilparasaram@gmail.com>
2021-08-18 17:19:07 +0530

    Remove deepcopy

--- mythril/laser/ethereum/transaction/concolic.py
+++ mythril/laser/ethereum/transaction/concolic.py
@@ -150,7 +150,7 @@ def _setup_global_state_for_execution(laser_evm, transaction) -> None:
         )
         new_node.constraints = global_state.world_state.constraints
 
-    global_state.world_state.transaction_sequence.append(deepcopy(transaction))
+    global_state.world_state.transaction_sequence.append(transaction)
     global_state.node = new_node
     new_node.states.append(global_state)
     laser_evm.work_list.append(global_state)
