2820aec72197ec6c237af8009ea00c5a6cce578f
norhh <nikhilparasaram@gmail.com>
2022-11-22 16:50:08 +0100

    Reformat

--- mythril/laser/ethereum/transaction/transaction_models.py
+++ mythril/laser/ethereum/transaction/transaction_models.py
@@ -28,7 +28,7 @@ class TxIdManager(object, metaclass=Singleton):
 
     def restart_counter(self):
         self._next_transaction_id = 0
-    
+
     def set_counter(self, tx_id):
         self._next_transaction_id = tx_id
 
