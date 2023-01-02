cd9d0a3e7f7b0cd68777b7a402221d16cc08304a
Nikhil Parasaram <nikhilparasaram@gmail.com>
2023-01-02 20:46:27 +0000

    Edit hashforether.sol

--- solidity_examples/hashforether.sol
+++ solidity_examples/hashforether.sol
@@ -3,13 +3,13 @@ pragma solidity 0.5.0;
 
 contract HashForEther {
 
-    function withdrawWinnings() public {
+    function withdrawWinnings() public payable {
         // Winner if the last 8 hex characters of the address are 0.
-        require(uint32(msg.sender) == 0);
-        _sendWinnings();
-     }
+        if (uint32(msg.sender) == 0)
+            _sendWinnings();
+    }
 
-     function _sendWinnings() public {
-         msg.sender.transfer(address(this).balance);
-     }
+    function _sendWinnings() public {
+        msg.sender.transfer(address(this).balance);
+    }
 }
