a8616dc87f41b308c5c0f1b69a2fcba5cebde99c
norhh <nikhilparasaram@gmail.com>
2023-03-06 16:43:37 +0000

    Remove assert

--- mythril/laser/ethereum/state/memory.py
+++ mythril/laser/ethereum/state/memory.py
@@ -200,7 +200,7 @@ class Memory:
         else:
             bv_key = simplify(convert_bv(key))
             if bv_key >= len(self):
-                assert False, "Error: Memory not extendedg"
+                return
             if isinstance(value, int):
                 assert 0 <= value <= 0xFF
             if isinstance(value, BitVec):
