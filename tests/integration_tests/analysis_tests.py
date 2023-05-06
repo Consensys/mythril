917e57db159d4fbb3d12a57e0aa383ee9315cc34
norhh <nikhilparasaram@gmail.com>
2023-05-06 19:11:16 +0530

    Save changes

--- tests/integration_tests/analysis_tests.py
+++ tests/integration_tests/analysis_tests.py
@@ -69,9 +69,9 @@ def test_analysis(file_name, tx_data, calldata):
 
 
 @pytest.mark.parametrize("file_name, tx_data, calldata", test_data)
-def test_analysis_delayed(file_name, tx_data, calldata):
+def test_analysis_pending(file_name, tx_data, calldata):
     bytecode_file = str(TESTDATA / "inputs" / file_name)
-    command = f"""python3 {MYTH} analyze -f {bytecode_file} -t {tx_data["TX_COUNT"]} -o jsonv2 -m {tx_data["MODULE"]} --solver-timeout 60000 --strategy delayed --no-onchain-data"""
+    command = f"""python3 {MYTH} analyze -f {bytecode_file} -t {tx_data["TX_COUNT"]} -o jsonv2 -m {tx_data["MODULE"]} --solver-timeout 60000 --strategy pending --no-onchain-data"""
     output = json.loads(output_of(command))
 
     assert len(output[0]["issues"]) == tx_data["ISSUE_COUNT"]
