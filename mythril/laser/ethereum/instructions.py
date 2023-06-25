2855b34a97943f5bb66ec074796bc8262443d00a
Nikhil Parasaram <nikhilparasaram@gmail.com>
2023-06-25 19:38:05 +0530

    Add push0 and push tests (#1781)
    * Add push0 and push tests
    
    * Fix issue with args
    
    * Add missing files

--- mythril/laser/ethereum/instructions.py
+++ mythril/laser/ethereum/instructions.py
@@ -284,13 +284,16 @@ class Instruction:
         :return:
         """
         push_instruction = global_state.get_current_instruction()
-        push_value = push_instruction["argument"]
+        push_value = push_instruction.get("argument", 0)
+
         try:
             length_of_value = 2 * int(push_instruction["opcode"][4:])
         except ValueError:
             raise VmException("Invalid Push instruction")
 
-        if type(push_value) == tuple:
+        if length_of_value == 0:
+            global_state.mstate.stack.append(symbol_factory.BitVecVal(0, 256))
+        elif type(push_value) == tuple:
             if type(push_value[0]) == int:
                 new_value = symbol_factory.BitVecVal(push_value[0], 8)
             else:
