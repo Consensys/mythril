04aae8659542a05e47929e83a1a76d3b787ad1f2
norhh <nikhilparasaram@gmail.com>
2023-08-17 03:51:04 +0100

    Change eth-rlp version

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Software Development :: Disassemblers",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
