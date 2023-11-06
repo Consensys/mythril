14d32a2b8c7fe27638d0ca02839eaed30d3fb113
Nikhil Parasaram <nikhilparasaram@gmail.com>
2023-11-06 07:01:49 +0000

    Fix a bug related to feature extraction (#1817)

---
+++ tests/testdata/input_contracts/SecureVault.sol
@@ -0,0 +1,120 @@
+
+pragma solidity ^0.8.20;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+abstract contract Ownable is Context {
+    address private _owner;
+
+    /**
+     * @dev The caller account is not authorized to perform an operation.
+     */
+    error OwnableUnauthorizedAccount(address account);
+
+    /**
+     * @dev The owner is not a valid owner account. (eg. `address(0)`)
+     */
+    error OwnableInvalidOwner(address owner);
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
+     */
+    constructor(address initialOwner) {
+        if (initialOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(initialOwner);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        if (owner() != _msgSender()) {
+            revert OwnableUnauthorizedAccount(_msgSender());
+        }
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        if (newOwner == address(0)) {
+            revert OwnableInvalidOwner(address(0));
+        }
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+contract SecureVault is Ownable {
+    // Event to emit when Ether is deposited
+    event Deposit(address indexed sender, uint256 amount);
+    // Event to emit when Ether is withdrawn
+    event Withdrawal(address indexed to, uint256 amount);
+
+    // Constructor that sets the initial owner of the SecureVault
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
+    // Function to deposit Ether into the contract. Anybody can deposit.
+    function deposit() external payable {
+        require(msg.value > 0, "Cannot deposit 0 Ether");
+        emit Deposit(_msgSender(), msg.value);
+    }
+
+    // Function that allows only the owner to withdraw Ether from the contract.
+    function withdraw(uint256 amount) external onlyOwner {
+        require(address(this).balance >= amount, "Insufficient balance to withdraw");
+        payable(owner()).transfer(amount);
+        emit Withdrawal(owner(), amount);
+    }
+
+    // Function to check the balance of the contract.
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
