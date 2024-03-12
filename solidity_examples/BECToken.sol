baa6068814c51514fec943a3cd896cd76804be68
suchsoon <silverjadebeauty@gmail.com>
2024-03-12 17:27:50 +0800

    chore: remove repetitive words
    Signed-off-by: suchsoon <silverjadebeauty@gmail.com>

--- solidity_examples/BECToken.sol
+++ solidity_examples/BECToken.sol
@@ -68,7 +68,7 @@ contract BasicToken is ERC20Basic {
 
   /**
   * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
+  * @param _owner The address to query the balance of.
   * @return An uint256 representing the amount owned by the passed address.
   */
   function balanceOf(address _owner) public returns (uint256 balance) {
